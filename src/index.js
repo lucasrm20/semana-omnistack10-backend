5161c66592f15580891cb838e9be95e8218fa66f
Lucas Rodrigues Moura <lucas.rodrigues20@outlook.com>
2020-01-19 11:07:15 -0300

    add dotenv

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express   = require('express');
 const mongoose  = require('mongoose');
 const cors      = require('cors');
-const routes    = require('./routes');
+
+require('dotenv').config();
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack:admin@cluster0-6ttrq.mongodb.net/week10?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser     : true,
   useUnifiedTopology  : true,
   useFindAndModify    : false,
@@ -14,6 +15,6 @@ mongoose.connect('mongodb+srv://omnistack:admin@cluster0-6ttrq.mongodb.net/week1
 
 app.use(cors());
 app.use(express.json());
-app.use(routes);
+app.use(require('./routes'));
 
 app.listen(3333);
